c5597c87f98383eb48655cd7beb4e8575ec35b72
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-11 14:16:44 +0300

    Created feign client

---
+++ src/main/java/com/microshop/stockmanagement/productcacheservice/feign/product/ProductServiceFeignClient.java
@@ -0,0 +1,15 @@
+package com.microshop.stockmanagement.productcacheservice.feign.product;
+
+import com.microshop.stockmanagement.productcacheservice.enums.Language;
+import com.microshop.stockmanagement.productcacheservice.response.InternalApiResponse;
+import com.microshop.stockmanagement.productcacheservice.response.ProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("${feign.product-service.name}")
+public interface ProductServiceFeignClient {
+
+    @GetMapping(value = "/api/1.0/product/{language}/get/productId")
+    InternalApiResponse<ProductResponse> getProduct(@PathVariable("language") Language language, @PathVariable("productId") Long productId);
+}
